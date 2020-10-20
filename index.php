bdf8b78f0d6b5b6390b5bd5c8947f8f26653a37b
Puagh <pablofeijo181@gmail.com>
2020-10-20 13:31:46 -0300

    Codigo Formateado.

--- index.php
+++ index.php
@@ -1,57 +1,58 @@
 <!DOCTYPE html>
 
 <html>
+
 <head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1, maximum-scale=1">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta id="theme-color" name="theme-color" content="#ffffff">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1, maximum-scale=1">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta id="theme-color" name="theme-color" content="#ffffff">
 
-    <base target="_blank">
+  <base target="_blank">
 
-    <title>Crami Endoscopia</title>
+  <title>Crami Endoscopia</title>
 
-    <link rel="icon" sizes="192x192" href="img/LogoICO.ico">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link rel="icon" sizes="192x192" href="img/LogoICO.ico">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/bulma.min.css">
+  <link rel="stylesheet" href="css/bulma.min.css">
 
 </head>
 
 <body>
 
-    <?php
-    session_start();
-    ?>
+  <?php
+  session_start();
+  ?>
 
-    <section class="hero is-link" style="height: 125px;">
-  <div class="hero-body">
-    <div class="container has-text-centered" style="margin-top: -25px;">
-      <h1 class="title "><img src="img/LogoCRAMI.png ">
-        Crami Endoscopia
-      </h1>
-      
+  <section class="hero is-link" style="height: 125px;">
+    <div class="hero-body">
+      <div class="container has-text-centered" style="margin-top: -25px;">
+        <h1 class="title "><img src="img/LogoCRAMI.png ">
+          Crami Endoscopia
+        </h1>
+
+      </div>
     </div>
-  </div>
-</section>
+  </section>
 
 
 
-<div class="container has-text-centered" id="container" style="padding-top: 15px; height: 400px;">
+  <div class="container has-text-centered" id="container" style="padding-top: 15px; height: 400px;">
 
 
-<form method="post" action="php/crear_carpeta.php"  ">
+    <form method="post" action="php/crear_carpeta.php" ">
 
    
-        <label class="label">Cedula de Identidad del paciente</label>
-        
-            <input class="input" type="text" placeholder="Sin puntos ni guiones" name="cedula" style="width: 20%;">
-             
-        
-        <button type="submit" class="button is-link">Ingresar</button>
-   
-</form>
+        <label class=" label">Cedula de Identidad del paciente</label>
+
+      <input class="input" type="text" placeholder="Sin puntos ni guiones" name="cedula" style="width: 20%;">
+
+
+      <button type="submit" class="button is-link">Ingresar</button>
+
+    </form>
 
 
 
@@ -59,33 +60,22 @@
 
 
 
-</div>
-<footer class="footer is-small" >
-  <div class="content has-text-centered" ">
-    <p style="font-size: 10px;">
-      <strong>-Endoscopia Web-</strong> Creado en HTML, CSS, JS y PHP -  Pablo Feijó.
-    </p>
   </div>
-</footer>
+  <footer class="footer is-small">
+    <div class="content has-text-centered" ">
+    <p style=" font-size: 10px;">
+      <strong>-Endoscopia Web-</strong> Creado en HTML, CSS, JS y PHP - Pablo Feijó.
+      </p>
+    </div>
+  </footer>
 
-    <div id="errorMsg"></div>
+  <div id="errorMsg"></div>
 
 
 
-<script src="js/main.js" async></script>
+  <script src="js/main.js" async></script>
 
 
 </body>
+
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
